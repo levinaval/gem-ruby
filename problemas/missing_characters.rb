3a05f25483f8e26078c63f0554fcda2073409ac7
levi <levisantosluna@gmail.com>
2021-05-10 16:25:06 -0300

    missing characters

---
+++ problemas/missing_characters.rb
@@ -0,0 +1,13 @@
+def missing_characters(string)
+  string.downcase!
+  alph_array = Array.new(26, false)
+
+  string.each_byte do |char|
+    alph_array[char - 'a'.ord] = true if char != ' '.ord
+  end
+
+  alph_array.all?(true)
+end
+
+# puts missing_characters('The quick brown fox jumps over the lazy dog') # true
+# puts missing_characters('The quick brown fox jumps over the dog') # false
